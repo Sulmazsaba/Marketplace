67f9d62c91850158ed92f9764d38a8479465c5f3
Sulmaz <Sulmaz@DESKTOP-MM1IDAG>
2021-09-21 08:31:51 +0430

    Add project files.

---
+++ Marketplace.Domain/ClassifiedAdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+   public class ClassifiedAdd
+    {
+        public Guid Id { get; private set; }
+
+
+        private Guid _ownerId { get; set; }
+        private decimal _price { get; set; }
+        private string _title { get; set; }
+
+        private string _text { get; set; }
+    }
+}
